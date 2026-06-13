5b563a0fa9817eb67ef6e18d04ce33952ed50911
rafeel <rafaelampaz6@gmail.com>
2026-06-13 14:10:53 -0300

    first commit gunicornio

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Google OAuth
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
